a822442417f84820108f580fd8296c0c437e20ad
Jose Germán Martínez <jgermanmm@gmail.com>
2023-06-07 23:52:21 -0400

    Proteccion de ruta getuser funcionando

--- src/features/user/routes/userRoute.ts
+++ src/features/user/routes/userRoute.ts
@@ -13,7 +13,7 @@ class UserRoutes {
     // Design Pattern Mediator: https://refactoring.guru/es/design-patterns/mediator
     // Design Pattern Prototype: https://refactoring.guru/es/design-patterns/prototype
     this.router.post('/signup', UserControllers.prototype.create);
-    this.router.get('/getusers', authMiddleware.checkAuthentication, UserControllers.prototype.getAllUsers);
+    this.router.get('/getusers', authMiddleware.verifyUser, UserControllers.prototype.getAllUsers);
     this.router.get('/getuser/:id', UserControllers.prototype.getUserById);
     this.router.put('/updateuser/:id', UserControllers.prototype.updateUser);
     this.router.delete('/deleteuser/:id', UserControllers.prototype.deleteUser);
--- src/shared/global/helpers/middlewares/protectRouteMiddleware.ts
+++ src/shared/global/helpers/middlewares/protectRouteMiddleware.ts
@@ -2,6 +2,10 @@ import { Request, Response, NextFunction } from 'express';
 import JWT from 'jsonwebtoken';
 import { config } from '@configs/configEnvs';
 import { NotAuthorizedError } from '@helpers/errors/notAuthorizedError';
+import { logger } from '@configs/configLogs';
+import Logger from 'bunyan';
+
+const log: Logger = logger.createLogger('auth');
 
 declare global {
     namespace Express {
@@ -19,12 +23,13 @@ interface AuthPayload {
 
 export class AuthMiddleware {
   public verifyUser(req: Request, _res: Response, next: NextFunction): void {
-    if (!req.session?.jwt) {
+    const token = req.header('Authorization');
+    if (!token) {
       throw new NotAuthorizedError('Token is not available. Please login again.');
     }
 
     try {
-      const payload: AuthPayload = JWT.verify(req.session?.jwt, config.JWT_TOKEN!) as AuthPayload;
+      const payload: AuthPayload = JWT.verify(token, config.JWT_TOKEN!) as AuthPayload;
       req.currentUser = payload;
     } catch (error) {
       throw new NotAuthorizedError('Token is invalid. Please login again.');
@@ -34,7 +39,9 @@ export class AuthMiddleware {
 
   public checkAuthentication(req: Request, _res: Response, next: NextFunction): void {
     if (!req.currentUser) {
+        log.info('Authentication is required to access this route')
       throw new NotAuthorizedError('Authentication is required to access this route.');
+      
     }
     next();
   }
